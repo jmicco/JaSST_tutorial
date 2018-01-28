95b05b491b16f0c4b70e2a89657475644ea8bd12
jmicco <parent.bank.app@gmail.com>
2018-01-28 14:11:07 -0800

    Add a query to count the transitions for each target

---
+++ count_transitions.sql
@@ -0,0 +1,40 @@
+#standardSQL
+#
+
+# Finds all tests that are probably flaky, having more than 2
+# transitions in a 1 month period.  NOTE: This may need to be tuned to
+# match your particular situation or data.
+#
+# Simply sort the result by timestamp of the commit or something that
+# tracks your branch, generate ROW_NUMBER(), and then join back against itself
+# to find the transitions.
+#
+# This query simply sorts by the number of state changes of the target
+# results.  Find a line that shows which tests are flaky,  this query
+# excludes any target where there have not been at least 2 transitions.
+#
+# Only include results of PASSED, FAILED, FLAKY and SKIPPED.  In our system
+# other results indicate problems running the tests or tests that we never ran
+#
+
+WITH result_values AS (
+  SELECT
+    ROW_NUMBER() OVER (PARTITION BY target_id ORDER BY changelist) AS row,
+    target_id,
+    changelist,
+    result
+  FROM `TestResultData.results`
+  WHERE result IN ('PASSED', 'FAILED', 'FLAKY', 'SKIPPED')
+  ORDER BY row
+)
+SELECT
+  A.target_id,
+  count(*) AS transition_count
+FROM result_values AS A
+  LEFT OUTER JOIN result_values AS B
+  ON A.target_id = B.target_id AND A.row = B.row-1
+WHERE A.result != B.result
+GROUP BY A.target_id
+HAVING transition_count > 2 #  <= Tune this based on your data set
+ORDER BY transition_count desc, A.target_id
+;
